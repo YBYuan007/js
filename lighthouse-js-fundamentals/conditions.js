914301a8cdc6fb2357cb18e9c2ace7a7bd7b670c
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-02 15:17:29 +0000

    add more conditional examples

--- lighthouse-js-fundamentals/conditions.js
+++ lighthouse-js-fundamentals/conditions.js
@@ -4,3 +4,5 @@ if (raining) {console.log("Dont forget your umbrella!");}
 if (cold) {console.log("make sure you pick out a scarf"); }
 
 console.log("now you are ready to go outside")
+
+if (!raining) { console.log("leave your umbrella at home!")}
